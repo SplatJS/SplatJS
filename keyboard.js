db75a69cf6441313c28291e83562d0bbadc77b41
Eric Lathrop <eric@ericlathrop.com>
2014-01-27 19:06:12 -0500

    Add WASD keyboard controls.

--- keyboard.js
+++ keyboard.js
@@ -41,7 +41,17 @@ var Splat = (function(splat, window) {
 			38: "up",
 			39: "right",
 			40: "down",
-			77: "m"
+			65: "a",
+			68: "d",
+			69: "e",
+			70: "f",
+			71: "g",
+			77: "m",
+			81: "q",
+			87: "w",
+			82: "r",
+			83: "s",
+			84: "t",
 		}
 	};
 	splat.KeyboardInput = KeyboardInput;
